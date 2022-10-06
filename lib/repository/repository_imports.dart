533e98c59884e43b9f9dfa2c6b5f5c1170f05e74
Eugene Vozhakov <dezgandes@gmail.com>
2022-10-06 19:08:30 +0300

    test comit to view work

--- lib/repository/repository_imports.dart
+++ lib/repository/repository_imports.dart
@@ -34,4 +34,3 @@ part 'fact_repository.dart';
 FactRepository factRepository = FactRepository();
 CardRepository cardRrepository = CardRepository();
 HiveRepository hiveRepository = HiveRepository();
-//test comit to view work
