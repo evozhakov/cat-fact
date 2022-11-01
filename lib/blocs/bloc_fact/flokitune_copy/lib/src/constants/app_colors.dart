cc26d68a3f7f584c86a13599d46d57c3446b56ce
Eugene Vozhakov <dezgandes@gmail.com>
2022-11-01 18:11:34 +0200

    final

--- lib/blocs/bloc_fact/flokitune_copy/lib/src/constants/app_colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color color1 = Color.fromRGBO(217, 15, 72, 1);
-  static const Color color2 = Color.fromRGBO(8, 18, 37, 1);
-  static const Color color3 = Color.fromRGBO(44, 46, 59, 1);
-}
--- lib/blocs/bloc_fact/flokitune_copy/lib/src/constants/app_strings.dart
+++
@@ -1,9 +0,0 @@
-class AppStrings {
-  static const String version = 'v 1.01';
-
-  static const String obscuringCharacter = '*';
-  static const String floki = '\nFlokiTune';
-
-  static const String lorem =
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.';
-}
